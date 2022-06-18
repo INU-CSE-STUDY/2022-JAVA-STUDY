6cfcb15b8816e0471f4b00e6053f02edb5bf1ee6
uuuo3o <o3ouuu@inu.ac.kr>
2022-06-18 21:32:42 +0900

    feat: Create ch03.uuuo3o
    자바의 정석 Chapter 03 연산자(operator)
    사칙 연산자, 나머지 연산자 파트

---
+++ src/inu/cse/ch03/uuuo3o/OperatorEx20.java
@@ -0,0 +1,9 @@
+package inu.cse.ch03.uuuo3o;
+
+public class OperatorEx20 {
+    public static void main(String[] args) {
+        System.out.println((-10)%8);
+        System.out.println(10%(-8));
+        System.out.println((-10)%(-8));
+    }
+}
