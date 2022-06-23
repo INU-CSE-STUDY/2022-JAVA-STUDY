79b2406dc36e2206914f1e038ffddc6b7f639f98
hg_yellow <jang010505@gmail.com>
2022-06-23 23:36:35 +0900

    feat: Upload ch03.hgyellow OperatorEx11~OperatorEx32
    ch03의 예제 문제 11번부터 32번까지의 문제들을 풀어서 올럈습니다.

---
+++ src/inu/cse/ch03/hgyellow/OperatorEx20.java
@@ -0,0 +1,9 @@
+package inu.cse.ch03.hgyellow;
+
+public class OperatorEx20 {
+    public static void main(String[] args) {
+        System.out.println(-10%8);
+        System.out.println(10%-8);
+        System.out.println(-10%-8);
+    }
+}
