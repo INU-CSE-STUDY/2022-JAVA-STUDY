baa5d3e7176c383b396ec97c7bd13dda4ed10401
uuuo3o <o3ouuu@inu.ac.kr>
2022-06-24 14:13:04 +0900

    feat: Create ch04.uuuo3o
    자바의 정석 Chapter 04 조건문과 반복문
    if문, if-else문, 중첩 if문 학습

---
+++ src/inu/cse/ch04/uuuo3o/FlowEx4.java
@@ -0,0 +1,25 @@
+package inu.cse.ch04.uuuo3o;
+
+import java.util.Scanner;
+
+public class FlowEx4 {
+    public static void main(String[] args) {
+        int score = 0;      // 점수를 저장하기 위한 변수
+        char grade = ' ';   // 학점을 저장하기 위한 변수, 공백으로 초기화
+
+        System.out.print("점수를 입력하세요. > ");
+        Scanner scnnaer = new Scanner(System.in);
+        score = scnnaer.nextInt();
+
+        if (score >= 90) {
+            grade = 'A';
+        } else if (score >= 80) {
+            grade = 'B';
+        } else if (score >= 70) {
+            grade = 'C';
+        } else {
+            grade = 'D';
+        }
+        System.out.println("당신의 학점은 " + grade + "입니다.");
+    }
+}
