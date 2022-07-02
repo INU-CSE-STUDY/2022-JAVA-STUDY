9acd4de7200368f18a419a74f7550bc691fa0bcd
uuuo3o <o3ouuu@inu.ac.kr>
2022-07-02 21:01:10 +0900

    feat: Create ch04.uuuo3o
    자바의 정석 Chapter 04 조건문과 반복문
    for 반복문 학습

---
+++ src/inu/cse/ch04/uuuo3o/FlowEx12.java
@@ -0,0 +1,13 @@
+package inu.cse.ch04.uuuo3o;
+
+public class FlowEx12 {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 5; i++)
+            System.out.println(i);
+
+        System.out.println();
+
+        for (int i = 1; i <= 5; i++)
+            System.out.print(i);
+    }
+}
---
+++ src/inu/cse/ch04/uuuo3o/FlowEx16.java
@@ -0,0 +1,12 @@
+package inu.cse.ch04.uuuo3o;
+
+public class FlowEx16 {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 5; i++) {
+            for (int j = 1; j <= 10; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
