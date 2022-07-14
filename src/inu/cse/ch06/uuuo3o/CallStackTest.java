ee6e5c0bf6931192fcbe4b11f14c83cadfee8be2
uuuo3o <o3ouuu@inu.ac.kr>
2022-07-15 00:15:18 +0900

    feat: Create ch04.uuuo3o
    자바의 정석 Chapter 06 객체지향 프로그래밍 Ⅰ
    변수와 메소드 학습

---
+++ src/inu/cse/ch06/uuuo3o/CallStackTest.java
@@ -0,0 +1,15 @@
+package inu.cse.ch06.uuuo3o;
+
+public class CallStackTest {
+    public static void main(String[] args) {
+        firstMethod();
+    }
+
+    static void firstMethod() {
+        secondMethod();
+    }
+
+    static void secondMethod() {
+        System.out.println("secondMethod()");
+    }
+}
