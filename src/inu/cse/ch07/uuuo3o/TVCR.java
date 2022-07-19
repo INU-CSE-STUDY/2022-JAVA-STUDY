7e5bc8ac8fc1b93dcc602ff27dd79934bd39b150
o3ouuu <o3ouuu@inu.ac.kr>
2022-07-19 12:16:15 +0900

    feat: Create ch07.uuuo3o
    자바의 정석 Chapter 07 객체지향 프로그래밍 Ⅱ
    상속 학습

---
+++ src/inu/cse/ch07/uuuo3o/TVCR.java
@@ -0,0 +1,30 @@
+package inu.cse.ch07.uuuo3o;
+
+class Tv2 {
+    boolean power;  // 전원상태 (on/off)
+    int channel;    // 채널
+
+    void power() { power = !power; }
+    void channelUp() { ++channel; }
+    void channelDown() { --channel; }
+}
+
+class VCR {
+    boolean power;
+    int counter = 0;
+    void power() { power = !power; }
+
+    void play() { /* 내용 생략 */ }
+    void stop() { /* 내용 생략 */ }
+    void rew() { /* 내용 생략 */ }
+    void ff() { /* 내용 생략 */ }
+}
+
+public class TVCR extends Tv2 {
+    VCR vcr = new VCR();
+
+    void play() { vcr.play(); }
+    void stop() { vcr.stop(); }
+    void rew() { vcr.rew(); }
+    void ff() { vcr.ff(); }
+}
