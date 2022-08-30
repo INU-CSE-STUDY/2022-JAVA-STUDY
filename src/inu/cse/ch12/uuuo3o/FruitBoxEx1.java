53672466077e0047ba5e754d3fd01556470958ae
uuuo3o <o3ouuu@inu.ac.kr>
2022-08-30 23:57:09 +0900

    feat: Create ch12.uuuo3o
    자바의 정석 Chapter 12 지네릭스, 열거형, 애너테이션
    지네릭스(Generics) 학습

---
+++ src/inu/cse/ch12/uuuo3o/FruitBoxEx1.java
@@ -0,0 +1,39 @@
+package inu.cse.ch12.uuuo3o;
+
+import java.util.ArrayList;
+
+public class FruitBoxEx1 {
+    public static void main(String[] args) {
+        Box<Fruit> fruitBox = new Box<Fruit>();
+        Box<Apple> appleBox = new Box<Apple>();
+        Box<Toy> toyBox = new Box<Toy>();
+//        Box<Grape> grapeBox = new Box<Fruit>();   // 타입 불일치로 인한 에러
+
+        fruitBox.add(new Fruit());
+        fruitBox.add(new Apple());
+
+        appleBox.add(new Apple());
+        appleBox.add(new Apple());
+//        appleBox.add(new Toy());  // appleBox에는 Apple만 넣을 수 있음
+
+        toyBox.add(new Toy());
+
+        System.out.println(fruitBox);
+        System.out.println(appleBox);
+        System.out.println(toyBox);
+    }
+}
+
+class Fruit { public String toString() { return "Fruit"; }}
+class Apple extends Fruit { public String toString() { return "Apple"; }}
+class Grape extends Fruit { public String toString() { return "Grape"; }}
+class Toy { public String toString() { return "Toy"; }}
+
+class Box<T> {
+    ArrayList<T> list = new ArrayList<T>();
+
+    void add(T item) { list.add(item); }
+    T get(int i) { return list.get(i); }
+    int size() { return list.size(); }
+    public String toString() { return list.toString(); }
+}
